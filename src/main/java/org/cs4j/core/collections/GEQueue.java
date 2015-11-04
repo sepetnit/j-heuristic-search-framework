aa11de597f330987ada76e6c4b4c65832b109975
Vitali Sepetnitsky <sepetnit@gmail.com>
2015-11-04 12:37:03 +0200

    GEQueue documented

--- src/main/java/org/cs4j/core/collections/GEQueue.java
+++ src/main/java/org/cs4j/core/collections/GEQueue.java
@@ -2,124 +2,171 @@ package org.cs4j.core.collections;
 
 import java.util.Comparator;
 
-public class GEQueue<E extends SearchQueueElement & RBTreeElement<E, E>> {
+/**
+ * A data structure for open and focal lists
+ *
+ * @param <E> Type of data to save in the list
+ */
 //public class GEQueue<E extends RBTreeElement<E, E> & MinHeapable> {
-  
-  private RBTree<E, E> open;
-  private BinHeap<E> focal;
-  private int id;
-  private Comparator<E> geComparator;
-  private static final int ADD = 0;
-  private static final int REMOVE = 1;
-  
-  private RBTreeVisitor<E> focalVisitor = new RBTreeVisitor<E>() {
-    @Override
-	public void visit(E e, int op) {
-      switch(op) {
-      case ADD:
-        if (e.getIndex(id) == -1) {
-          focal.add(e);        
+public class GEQueue<E extends SearchQueueElement & RBTreeElement<E, E>> {
+    private RBTree<E, E> open;
+    private BinHeap<E> focal;
+    private int id;
+    private Comparator<E> geComparator;
+
+    private static final int ADD = 0;
+    private static final int REMOVE = 1;
+
+    private RBTreeVisitor<E> focalVisitor = new RBTreeVisitor<E>() {
+        @Override
+        public void visit(E e, int op) {
+            switch(op) {
+                case ADD: {
+                    // Add to focal if the element isn't already contained there
+                    if (e.getIndex(GEQueue.this.id) == -1) {
+                        GEQueue.this.focal.add(e);
+                    }
+                    break;
+                }
+                case REMOVE: {
+                    // Remove from focal if the element is contained there
+                    assert e.getIndex(GEQueue.this.id) != -1;
+                    GEQueue.this.focal.remove(e);
+                    break;
+                }
+            }
         }
-        break;
-      case REMOVE:
-        assert e.getIndex(id) != -1;
-        focal.remove(e);
-        break;
-      }
-    }
-  };
-  
-  public GEQueue(Comparator<E> openComparator, Comparator<E> geComparator,
-      Comparator<E> focalComparator, int id) {
-    this.id = id;
-    this.geComparator = geComparator;
-    focal = new BinHeap<E>(focalComparator, id);
-    open = new RBTree<E, E>(openComparator, geComparator);
-  }
+    };
 
-  public boolean isEmpty() {
-    return open.peek() == null;
-  }
-
-  public void add(E e, E oldBest) {
-    assert e.getNode() == null;
-    open.insert(e, e);
-    // assumes oldBest is still valid
-    if (geComparator.compare(e, oldBest) <= 0) {
-      focal.add(e);
+    public GEQueue(Comparator<E> openComparator,
+                   Comparator<E> geComparator,
+                   Comparator<E> focalComparator,
+                   int id) {
+        this.id = id;
+        this.geComparator = geComparator;
+        this.focal = new BinHeap<E>(focalComparator, id);
+        this.open = new RBTree<E, E>(openComparator, geComparator);
     }
-    assert e.getNode() != null;
-  }
-  
-  public void updateFocal(E oldBest, E newBest, int fHatChange) {
-    assert newBest != null;
-    assert newBest.getNode() != null;
+
+    public boolean isEmpty() {
+        return this.open.peek() == null;
+    }
+
+    /**
+     * Add to queue
+     *
+     * @param e The element to add
+     * @param oldBest The old element which was the 'best' one in the queue
+     */
+    public void add(E e, E oldBest) {
+        assert e.getNode() == null;
+        this.open.insert(e, e);
+        // assumes oldBest is still valid
+        if (this.geComparator.compare(e, oldBest) <= 0) {
+            this.focal.add(e);
+        }
+        assert e.getNode() != null;
+    }
+
+    public void updateFocal(E oldBest, E newBest, int fHatChange) {
+        assert newBest != null;
+        assert newBest.getNode() != null;
         
-    // did best f^ change?  
-    if (oldBest == null || fHatChange != 0) {
-      // did best f^ go down?
-      if (oldBest != null && fHatChange < 0) {
-//          try {
-        open.visit(newBest, oldBest, REMOVE, focalVisitor);
-//          }
-//         catch (ArrayIndexOutOfBoundsException e){
-//             System.out.print(oldBest);
-//              System.out.print("Error");
-//         }
-      }
-      // then best f^ when up
-      else if (oldBest == null || oldBest.getNode() == null) {
-        open.visit(oldBest, newBest, ADD, focalVisitor);
-      }
+        // did best f^ change?
+        if (oldBest == null || fHatChange != 0) {
+            // did best f^ go down?
+            if (oldBest != null && fHatChange < 0) {
+                // try {
+                    this.open.visit(newBest, oldBest, REMOVE, this.focalVisitor);
+                // }
+                // catch (ArrayIndexOutOfBoundsException e){
+                //    System.out.print(oldBest);
+                //    System.out.print("Error");
+                //   }
+            // then best f^ when up
+            } else if (oldBest == null || oldBest.getNode() == null) {
+                this.open.visit(oldBest, newBest, ADD, focalVisitor);
+            }
+        }
+        //verifyFocal();
     }
-    //verifyFocal();
-  }
   
-  /*public void verifyFocal() {
-    E best = open.peek();
-    // make sure all nodes in focal are close to best
-    List<E> heap = ((BinHeap)focal).heap;
-    for (E o : heap) {
-      assert geComparator.compare(o, best) <= 0;
-    }
-    // make sure all nodes in open that are close to best are in focal
-    List<E> rbNodes = open.getValues();
-    for (E o : rbNodes) {
-      assert geComparator.compare(o, best) > 0 || o.getIndex(id) != -1;
-    }
-  }*/
-  
-  public void remove(E e) {
-    assert e.getNode() != null;
-    open.delete(e);
-    if (e.getIndex(id) != -1) {
-      focal.remove(e);
-    }
-  }
-  
-  public E pollOpen() {
-    E e = open.poll();
-    if (e != null && e.getIndex(id) != -1) {
-      focal.remove(e);
-    }
-    return e;
-  }
-  
-  public E pollFocal() {
-    E e = focal.poll();
-    if (e != null) {
-      assert e.getNode() != null;
-      open.delete(e);
-    }
-    return e;
-  }
+    //public void verifyFocal() {
+    //    // Take the best f^
+    //    E best = this.open.peek();
+    //    // make sure all nodes in focal are close to best
+    //    List<E> heap = ((BinHeap)this.focal).heap;
+    //    for (E o : heap) {
+    //      assert this.geComparator.compare(o, best) <= 0;
+    //    }
+    //    // make sure all nodes in open that are close to best are in focal
+    //    List<E> rbNodes = this.open.getValues();
+    //    // Go over all the nodes in open and for every node check that:
+    //    // The node is not suitable to be in focal or The node is in focal
+    //    for (E o : rbNodes) {
+    //        assert this.geComparator.compare(o, best) > 0 || o.getIndex(this.id) != -1;
+    //    }
+    //}
 
-  public E peekOpen() {
-    return open.peek();
-  }
-  
-  public E peekFocal() {
-    return focal.peek();
-  }
-  
+    /**
+     * Removes a node from OPEN and also from FOCAL
+     *
+     * @param e The node to remove
+     */
+    public void remove(E e) {
+        assert e.getNode() != null;
+        this.open.delete(e);
+        if (e.getIndex(id) != -1) {
+            this.focal.remove(e);
+        }
+    }
+
+    /**
+     * Polling a node from OPEN
+     *
+     * NOTE: Removes the node from FOCAL!
+     *
+     * @return The extracted node
+     */
+    public E pollOpen() {
+        E e = this.open.poll();
+        if (e != null && e.getIndex(id) != -1) {
+            this.focal.remove(e);
+        }
+        return e;
+    }
+
+    /**
+     * Polling a node from FOCAL
+     *
+     * NOTE: Removes the node from OPEN!
+     *
+     * @return The extracted node
+     */
+    public E pollFocal() {
+        E e = this.focal.poll();
+        if (e != null) {
+            assert e.getNode() != null;
+            this.open.delete(e);
+        }
+        return e;
+    }
+
+    /**
+     * Peeks a node from OPEN (without removing it)
+     *
+     * @return The extracted node
+     */
+    public E peekOpen() {
+        return this.open.peek();
+    }
+
+    /**
+     * Peeks a node from FOCAL (without removing it)
+     *
+     * @return The extracted node
+     */
+    public E peekFocal() {
+        return this.focal.peek();
+    }
 }
