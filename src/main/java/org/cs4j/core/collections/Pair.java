ee0affbca2544d7e826d96c43e5da5404e75fe1b
sepetnit <sepetnit@gmail.com>
2015-11-08 13:13:43 +0200

    Added toString to general Pair object

--- src/main/java/org/cs4j/core/collections/Pair.java
+++ src/main/java/org/cs4j/core/collections/Pair.java
@@ -18,4 +18,9 @@ public class Pair<Key,Value> {
 
     public void setKey(Key key){ this.key = key; }
     public void setValue(Value value){ this.value = value; }
+
+    @Override
+    public String toString() {
+        return "(" + this.key + ", " + this.value + ")";
+    }
 }
